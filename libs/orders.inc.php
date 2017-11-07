775c09d0b1d0c902c6a1cd585309ad0245113cff
David Slater <dslater@dslater-lnx.igb.illinois.edu>
2017-11-07 15:07:45 -0600

    added gitignore updated orders.inc.php have example settings file

--- libs/orders.inc.php
+++ libs/orders.inc.php
@@ -45,10 +45,16 @@ function getOrdersReport($db,$month,$year) {
 	$sql = "SELECT tbl_orders.orders_id as 'Order Number', tbl_orders.orders_email as 'Email', ";
 	$sql .= "tbl_orders.orders_name as 'Full Name', tbl_orders.orders_timeCreated as 'Date', ";
 	$sql .= "tbl_orders.orders_cfop as 'CFOP', tbl_orders.orders_activityCode as 'Activity Code', ";
-	$sql .= "tbl_orders.orders_totalCost as 'Cost' "; 
+	$sql .= "tbl_orders.orders_totalCost as 'Cost' ";
+	$sql .= "tbl_paperTypes.paperTypes_name as 'Paper Type', tbl_paperTypes.paperTypes_cost as 'Paper Type Cost (per Inch)', ";
+	$sql .= "tbl_finishOptions.finishOptions_name as 'Finish Option', tbl_finishOptions.finishOptions_cost as 'Finish Option Cost', ";
+	$sql .= "tbl_rushOrder.rushOrder_name as 'Rush Order', tbl_rushOrder.rushOrder_cost as 'Rush Order Cost', ";
+	$sql .= "tbl_posterTube.posterTube_name as 'Poster Tube', tbl_posterTube.posterTube_cost as 'Poster Tube Cost' " 
 	$sql .= "FROM tbl_orders LEFT JOIN tbl_status ON tbl_orders.orders_statusId=tbl_status.status_id ";
 	$sql .= "LEFT JOIN tbl_paperTypes ON tbl_orders.orders_paperTypesId=tbl_paperTypes.paperTypes_id ";
 	$sql .= "LEFT JOIN tbl_finishOptions ON tbl_orders.orders_finishOptionsId=tbl_finishOptions.finishOptions_id "; 
+	$sql .= "LEFT JOIN tbl_posterTube ON tbl_orders.orders_posterTubeId=tbl_posterTube.posterTube_id ";
+	$sql .= "LEFT JOIN tbl_rushOrder ON tbl_orders.orders_rushOrderId=tbl_rushOrder.rushOrder_id ";
 	$sql .= "WHERE (YEAR(orders_timeCreated)='" . $year . "' AND month(orders_timeCreated)='" . $month . "') ";
 	$sql .= "AND status_name='Completed' ";
 	$sql .= "ORDER BY orders_id ASC";
