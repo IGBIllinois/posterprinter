89af3db94b61f5c938423e9c8697e13fc643a2c5
David Slater <dslater@dslater-lnx.igb.illinois.edu>
2023-04-11 13:47:43 -0500

    Updated stats orders per month to have year selection

--- html/admin/stats_OrdersPerMonth.php
+++ html/admin/stats_OrdersPerMonth.php
@@ -7,32 +7,55 @@ require_once 'includes/header.inc.php';
 if (isset($_GET['year'])) { $year = $_GET['year']; }
 else { $year = date('Y'); }
 
-$previousYear = $year -1;
-$nextYear =$year +1;
-$startDate = $year . "/01/01";
-$endDate = $year . "/12/31";
+$previous_year = $year -1;
+$next_year =$year +1;
+$start_date = $year . "/01/01";
+$end_date = $year . "/12/31";
 
-$stats = new statistics($db,$startDate,$endDate);
+//////Year////////
+$min_year = functions::get_minimal_year($db);
+$year_html = "<select class='form-control' name='year'>";
+for ($i=$min_year; $i<=date("Y");$i++) {
+        if ($i == $year) { $year_html .= "<option value='" . $i . "' selected='true'>" . $i . "</option>"; }
+        else { $year_html .= "<option value='" . $i . "'>" . $i . "</option>"; }
+}
+$year_html .= "</select>";
+
+$url_navigation = html::get_url_navigation_year($_SERVER['PHP_SELF'],$year);
+
+
+$stats = new statistics($db,$start_date,$end_date);
 $graph_type = "orders_per_month";
 
 ?>
 <h3>Orders Per Month - <?php echo $year; ?></h3>
-<hr>
-<ul class='pagination justify-content-center'>
-<li class='page-item'><a class='page-link' href='stats_OrdersPerMonth.php?year=<?php echo $previousYear; ?>'>Previous</a></li>
-<?php
-        $next_year = strtotime('+1 day', strtotime($endDate));
-	$today = mktime(0,0,0,date('m'),date('d'),date('y'));
+<form class='form-inline' action='<?php echo $_SERVER['PHP_SELF']; ?>' method='get'>
+<div class='form-group'>
+        <label for='year'>Year:</label>
+        &nbsp; <?php echo $year_html; ?>
+</div>
+&nbsp;
+<div class='form-group'>
+        <input type='submit' class='btn btn-primary' value='Get Records'>
+</div>
+</form>
+<p>
+<div class='row'>
+        <div class='col-sm-12 col-md-12 col-lg-12 col-xl-12'>
+        <a class='btn btn-sm btn-primary' href='<?php echo $url_navigation['back_url']; ?>'>Previous Year</a>
 
-        if ($next_year > $today) {
-                echo "<li class='page-item disabled'><a class='page-link' href='#'>Next</a></li>";
-        }
-        else {
-                echo "<li class='page-item'><a class='page-link' href='stats_OrdersPerMonth.php?year=" . $nextYear  . "'>Next</a></li>";
-        }
-?>
+        <?php
+                if ($next_year > $year) {
+                        echo "<div class='float-right'><a class='btn btn-sm btn-primary' onclick='return false;'>Next Year</a></div>";
+                }
+                else {
+                        echo "<div class='float-right'><a class='btn btn-sm btn-primary' href='" . $url_navigation['forward_url'] . "'>Next Year</a></div>";
+                }
+        ?>
+        </div>
+</div>
+<p>
 
-</ul>
 <table class='table table-bordered table-condenesed'>
 
     <tr><td>Yearly Total:</td><td>$<?php echo $stats->pretty_cost(); ?></td></tr>
