250fbc92aea4897eb80ff8c71059ea4e9a72f0f9
David Slater <dslater@dslater-lnx.igb.illinois.edu>
2025-04-23 09:21:41 -0500

    Fixed rebase

--- includes/app.inc.php
+++ includes/app.inc.php
@@ -3,11 +3,7 @@
 Store Application Variables
 */
 
-<<<<<<< HEAD
-define("app_version","1.3.6");
-=======
 define("app_version","1.3.7");
->>>>>>> a15a0db (Updated jpgraph to use mitoteam/jpgraph)
 
 
 
